f362ba9d9bdb983a15d954468ba0d2c8a6637f49
Maria Fredriksson <mf223wk@student.lnu.se>
2023-10-30 12:00:46 +0100

    Change input form to dispatch event for every input + delete OK button

--- src/js/components/input-form/input-form.js
+++ src/js/components/input-form/input-form.js
@@ -18,9 +18,8 @@ template.innerHTML = `
     }
   </style>
   <div class="container">
-    <form id="input-form">
+    <form>
       <input id="input-text" type="text" placeholder="Write your text here">
-      <button class="submit-btn" type="submit">OK</button>
     </form>
   </div>
 `
@@ -29,7 +28,6 @@ customElements.define('input-form',
    * Represents a input-form element.
    */
   class extends HTMLElement {
-    #inputForm
     #inputText
 
     /**
@@ -40,14 +38,12 @@ customElements.define('input-form',
 
       this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
 
-      this.#inputForm = this.shadowRoot.querySelector('#input-form')
       this.#inputText = this.shadowRoot.querySelector('#input-text')
 
       // Set the focus to the input field from the start
       this.#inputText.focus()
 
-      // Listen for when a text has been submitted
-      this.#inputForm.addEventListener('submit', event => this.#textSubmitted(event))
+      this.#inputText.addEventListener('input', event => this.#textSubmitted(event))
     }
 
     /**
@@ -56,9 +52,6 @@ customElements.define('input-form',
      * @param {object} event - The event object.
      */
     #textSubmitted (event) {
-      // Prevent the page from reloading when the form is submitted
-      event.preventDefault()
-
       // ^^ Should I check if the input field is empty here or if the text contains invalid characters etc?
 
       // Dispatch a custom event that contains the text that was submitted
